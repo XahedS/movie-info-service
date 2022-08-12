cbb6660577180dec20d3d569561291342c850585
unknown <zahidsaleem0440@gmail.com>
2022-08-12 20:53:25 +0500

    Initial commit This is the Helper microservice working on Port 8082 and collecting movie info from theMovieDB.the movie id is passed ot it from rating data service. It is eureka client and uses properties file in yml format.

---
+++ src/main/java/example/microservice/movieinfoservice/models/MovieSummary.java
@@ -0,0 +1,27 @@
+package example.microservice.movieinfoservice.models;
+
+public class MovieSummary {
+
+    private String title;
+    private String overview;
+
+    public MovieSummary(){
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+}
